9d80a297575d2cdb3e569bc801602b826fa69ddb
Kevin Pacheco <k.pacheco@me.com>
2020-03-30 16:59:50 -0700

    Added 2D Space Shooter project.

---
+++ Assets/App/Scripts/Ship.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour {
+	public float speed = 1.0F; // moves 'speed' units per second... (speed)
+	public float rotation = 90.0F; // rotates 'rotation' amount of degrees per second.
+
+	private void Update() {
+		float xAxis = Input.GetAxis("Horizontal");
+		float yAxis = Input.GetAxis("Vertical");
+
+		// rotate logic...
+		Quaternion zRotation = Quaternion.Euler(0.0F, 0.0F, this.rotation * -xAxis * Time.deltaTime);
+
+		this.transform.rotation *= zRotation;
+
+		// move logic....
+		Vector3 position = this.transform.position;
+
+		Vector3 forwardDelta = Vector3.up * yAxis * Time.deltaTime * this.speed;
+
+		position += this.transform.rotation * forwardDelta;
+
+		this.transform.position = position;
+	}
+}
